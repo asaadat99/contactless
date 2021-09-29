f17b92b3a3deba58d1dd8a46e2310c5108553bfa
asaadat <asaadat@umich.edu>
2021-09-29 16:59:22 -0400

    first commit

---
+++ Contactless/ContactlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactlessApp.swift
+//  Contactless
+//
+//  Created by Affaan Saadat on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contactless/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contactless
+//
+//  Created by Affaan Saadat on 9/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
